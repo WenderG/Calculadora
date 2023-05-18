6df062d2e27e459704795b8465fb745acad84db1
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-05-18 14:35:41 -0300

    :test_tube: test: Testando função recursiva

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,15 @@
 public class Calculadora{
     public static void main(String [] args) {
+        int decimal = 3;
+        System.out.printf("O número %d em binário é: ", decimal);
+        converteBinario(decimal);
+        System.out.println();
+    }
+
+    public static void converteBinario(int n) {
+        if (n > 1)
+            converteBinario(n / 2);
         
+        System.out.print(n % 2);
     }
 }
